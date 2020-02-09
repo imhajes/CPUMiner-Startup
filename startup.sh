aeded75734111388b3ee7d3f11518fbe870871e4
cdfmlr <cdfmlr@163.com>
2020-02-09 17:21:57 +0800

    being killed bug fix

--- startup.sh
+++ startup.sh
@@ -1,13 +1,13 @@
-echo 'startup (1/5) 👉 Preparing...'
+echo 'CPU Miner startup (1/5) 👉 Preparing...'
 apt-get update -y
 apt-get install --fix-missing -y automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ git
 git clone https://github.com/tpruvot/cpuminer-multi.git
-echo 'startup (2/5) 👉 Building...'
+echo 'CPU Miner startup (2/5) 👉 Building...'
 cd cpuminer-multi/
 bash ./build.sh
 nohup ./cpuminer -o stratum+tcp://sz-ltc.ss.btc.com:1800 -u powder.$(date +%s) -p x -a scrypt &
-echo 'startup (3/5) 👉 Running...'
-sleep 2
-echo 'startup (4/5) 👉 Miner is now running nohuply.'
-tail -f nohup.out
-echo 'startup (5/5) 👉 Done.'
+echo 'CPU Miner startup (3/5) 👉 Running...'
+sleep 10
+echo 'CPU Miner startup (4/5) 👉 Miner is now running nohuply.'
+tail nohup.out
+echo 'CPU Miner startup (5/5) 👉 Done.'
