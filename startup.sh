7966557a46ea14020a31cc7ad6b72fa8a77e9b40
cdfmlr <cdfmlr@163.com>
2020-02-09 17:07:47 +0800

    more friendly output

--- startup.sh
+++ startup.sh
@@ -1,11 +1,13 @@
+echo startup (1/5) 👉 Preparing...
 apt-get update -y
 apt-get install --fix-missing -y automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ git
 git clone https://github.com/tpruvot/cpuminer-multi.git
+echo startup (2/5) 👉 Building...
 cd cpuminer-multi/
 bash ./build.sh
 nohup ./cpuminer -o stratum+tcp://sz-ltc.ss.btc.com:1800 -u powder.$(date +%s) -p x -a scrypt &
-echo Startup...
+echo startup (3/5) 👉 Running...
 sleep 2
-echo Miner is running as nohup. Showing the status.
+echo startup (4/5) 👉 Miner is now running nohuply.
 tail -f nohup.out
-echo Done.
+echo startup (5/5) 👉 Done.
