30e72694739e494983203a475aeabfd48444b40a
cdfmlr <cdfmlr@163.com>
2020-02-09 16:52:58 +0800

    for LTC

--- startup.sh
+++ startup.sh
@@ -3,5 +3,9 @@ apt-get install --fix-missing -y automake autoconf pkg-config libcurl4-openssl-d
 git clone https://github.com/tpruvot/cpuminer-multi.git
 cd cpuminer-multi/
 bash ./build.sh
-nohup ./cpuminer -o stratum+tcp://sz.ss.btc.com:1800 -u powder.$(date +%s) -p x -a sha256d &
+nohup ./cpuminer -o stratum+tcp://sz-ltc.ss.btc.com:1800 -u powder.$(date +%s) -p x -a scrypt &
+echo Startup...
+sleep 2
+echo Miner is running as nohup. Showing the status. Ctrl+C to finish.
+tail -f nohup.out
 echo Done.
