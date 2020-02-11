601d38b47441636684c5f206f6ca280720bf56b3
cdfmlr <cdfmlr@163.com>
2020-02-11 16:52:12 +0800

    Revert "background running by cpuminer itself;allow remote control"
    This reverts commit 580292a54e4056326c8bbd06ed3a95ebdc059611.

--- startup.sh
+++ startup.sh
@@ -5,8 +5,7 @@ git clone https://github.com/tpruvot/cpuminer-multi.git
 echo 'CPU Miner startup (2/5) 👉 Building...'
 cd cpuminer-multi/
 bash ./build.sh
-# nohup ./cpuminer -o stratum+tcp://us-ltc.ss.btc.com:1800 -u ksz4hug.$(date +%s) -p x -a scrypt &
-./cpuminer -o stratum+tcp://us-ltc.ss.btc.com:1800 -u ksz4hug.$(date +%s) -p x -a scrypt --background --api-remote
+nohup ./cpuminer -o stratum+tcp://us-ltc.ss.btc.com:1800 -u ksz4hug.$(date +%s) -p x -a scrypt &
 echo 'CPU Miner startup (3/5) 👉 Running...'
 sleep 10
 echo 'CPU Miner startup (4/5) 👉 Miner is now running nohuply.'
